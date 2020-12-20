05c661ccefa04b9f81a873d8d8e5d613ec9dcbfb
Dr4kk0nnys <renatoversianidrakk@gmail.com>
2020-12-20 11:19:19 -0300

    Solved the problem with the templates not being rendered / Solved the other issues as well, such as: The routes were not being called / The views were not being passed to the dist/

--- backend/src/routes/polls.ts
+++ backend/src/routes/polls.ts
@@ -3,7 +3,8 @@ const router = express.Router()
 
 router.post('/', (req, res) => {
     console.log(req.body);
-    res.redirect('http://localhost:5500/frontend/index.html')
+    res.send('Hello World');
+    // res.redirect('http://localhost:5500/frontend/index.html')
 });
 
 export default router;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 
 // Routes
-import index from './routes/index';
+import polls from './routes/polls';
 
 // Config
 const app = express();
@@ -12,7 +13,11 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 // Routes
-app.use('/index', index);
+app.get('/', (req, res) => res.render('index'));
+app.use('/polls', polls);
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
