d107d4df9a5f9f7f9e169e279d159efb18f398f1
Dr4kk0nnys <renatoversianidrakk@gmail.com>
2020-12-20 11:23:16 -0300

    Removed the /backend and /frontend and now it is all backend ( always has been space man meme )

--- frontend/src/utils/post_data.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('submit').addEventListener('click', () => {
-    const elements = document.getElementsByClassName('input_radio_polls');
-
-    for (const element of elements) {
-        console.log(element);
-    }
-});
